b0ef473e843e0bcd59f513fe1619a736e6512665
harithapotla <109097690+harithapotla@users.noreply.github.com>
2023-03-16 12:45:55 +0530

    after complition handleDelete function

--- src/components/Singletodo.tsx
+++ src/components/Singletodo.tsx
@@ -11,17 +11,32 @@ type Props = {
     setTodos:React.Dispatch<React.SetStateAction<Todo[]>>
 }
 const Singletodo = ({todo,todos,setTodos}:Props)=>{
+    
+
+    
+    const handleDelete = (id:number)=>{
+        setTodos(todos.filter((todo=>todo.id!==id)))
+    }
+    
+    const handleDone = (id:number)=>{
+        setTodos(todos.map((todo)=>todo.id===id?{...todo, isDone : !todo.isDone} :todo))
+    }
     return(
         <form className="todos_single">
-            <span className="todos_single--text">{todo.todo}</span>
+            <span className="todos_single--text">
+                
+                {todo.isDone ? (
+                    <s className="todos_single--text">{todo.todo}</s>
+                ) :(<span className="todos-single--text">{todo.todo}</span>)
+                }</span>
             <div>
                 <span className="icon">
                 <AiFillEdit/>
                 </span>
-                <span className="icon">
+                <span className="icon"  onClick={()=>handleDelete(todo.id)}>
                 <AiFillDelete/>
                 </span>
-                <span className="icon">
+                <span className="icon" onClick={()=>handleDone(todo.id)}>
                 <MdDone/>
                 </span>
             </div>
